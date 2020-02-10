9087e27eb31cf45c561437046239ec2c58cc9939
Neon <njuarez@hermescorp.io>
2020-02-10 13:28:15 -0600

    error_component

---
+++ src/components/error.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="left"
+          justify="center"
+        >
+        <div class="flex display-4 font-weight-bold">
+            <p>Something went wrong</p>
+        </div>
+        <v-divider></v-divider>
+        <div class="flex display-2 font-weight-light">
+            <p>Please log in again </p>
+        </div> 
+        </v-row>
+      </v-container>
+    </v-content>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+    created() {
+      this.$vuetify.theme.dark = true;
+    }
+}
+</script>
